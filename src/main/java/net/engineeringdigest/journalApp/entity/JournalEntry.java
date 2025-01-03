4e499b7238495bcb393e0bcb2f80a5c5f5f9eee3
vallaksa <149544390+vallaksa@users.noreply.github.com>
2025-01-02 21:30:10 -0500

    Updated the Response Entity and Integrated Lombok

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalApp.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "Journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -15,36 +17,4 @@ public class JournalEntry {
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
